6e3268de2932e289747fc0572fcddb64ec4c3db4
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-03-14 21:47:51 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+
 import '@styles/globals.css'
 
+import Footer from '../components/Footer';
+
+import Hamburger from '../components/Hamburger';
+import { PageTransition } from "../components/PageTransition";
+
+
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return ( 
+
+      <PageTransition>  
+  <Hamburger />
+
+  
+  <Component {...pageProps} />
+
+  <Footer />
+
+   </PageTransition>
+  
+
+  );
 }
 
 export default Application
