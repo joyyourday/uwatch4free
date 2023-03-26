415bee2300569315d6dc176ca6cf0ac5aef2261d
groupincworld <118275411+joyyourday@users.noreply.github.com>
2023-03-27 01:17:50 +0530

    update

--- public/propler/ads.js
+++ public/propler/ads.js
@@ -1,1 +1,1 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5741273,document.body||document.documentElement)
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',4903040,document.body||document.documentElement)
